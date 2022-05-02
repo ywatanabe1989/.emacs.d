8c2909dc9579c78d6f3ef13d84e5fa109ce8ca3b
Yusuke Watanabe <42527473+ywatanabe1989@users.noreply.github.com>
2022-05-02 12:43:04 +0900

    Update install_the_latest_emacs_on_ubuntu.sh

--- docs/install_the_latest_emacs_on_ubuntu.sh
+++ docs/install_the_latest_emacs_on_ubuntu.sh
@@ -13,7 +13,7 @@ git clone git://git.sv.gnu.org/emacs.git
 sudo apt install build-essential libgtk-3-dev libgnutls28-dev libtiff5-dev libgif-dev libjpeg-dev libpng-dev libxpm-dev libncurses-dev texinfo
 cd emacs
 ./autogen.sh
-./configure --prefix=/opt/emacs-29.0.50 --with-mailutils 
+./configure --prefix=/opt/emacs-latest --with-mailutils 
 make -j 12
 sudo make install
 
